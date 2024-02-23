de13e91588b81e62a6088d2cdc3165686314750a
Tushar shitole <153802284+shitoletushar3132@users.noreply.github.com>
2024-02-23 09:26:10 +0530

    Add files via upload

---
+++ day 15/script.js
@@ -0,0 +1,139 @@
+// console.log("3132");
+
+// let a =10;
+// let b=5;
+// console.log("sum is :", a+b);
+
+// let pencilPrice = 10;
+// let pen = 10;
+// // let op = (`the total price is : ${pencilPrice + pen}`)
+// console.log((`the total price is : ${pencilPrice + pen}`));
+
+let a = 10;
+let b = 5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a**b);
+// console.log(a/b);
+// console.log(a%b);
+
+// console.log(a++);
+// console.log(++a);
+// console.log(a--);
+// console.log(--a);
+
+// console.log(a>b);
+// console.log(a>=b);
+// console.log(a<b);
+// console.log(a<=b);
+// console.log(a==b);
+// console.log(a!=b);
+
+let A = "10";
+
+// console.log(a ==A);
+// console.log(a===A);
+
+// alert("hello");
+// prompt("get A");
+
+// console.log('a' < 'A');
+
+// age = prompt("enter a age");
+
+// if(age > 18){
+//     console.log("you are able to vote")
+// }else{
+//     console.log("you are not able to vote");
+// }
+
+// let firstName = "tshar";
+
+// if(firstName == "tushar"){
+//     console.log(`welcome to my page ${firstName}`);
+// }
+
+// let tafficLightColor = "yellow";
+
+// if(tafficLightColor == "red"){
+//     console.log(`stop!`);
+// }else if(tafficLightColor == "green"){
+//     console.log(`go>>`);
+// }else if(tafficLightColor == "yellow"){
+//     console.log('slow down!>');
+// }
+
+// let marks = 35;
+
+// if(marks >= 35){
+//     console.log("you are pass");
+// }
+// if(marks >= 35 && marks <= 40){
+//     console.log("you're grade is E");
+// }
+
+// let str = "dpache";
+
+// if((str[0] == 'a' || str[0]=='A') && str.length>3){
+//     console.log("good string");
+// }else{
+//     console.log("bad string");
+// }
+
+// if(true){
+//     console.log("it is ");
+// }else{
+//     console.log('dfj');
+// }
+
+// let color = "dsf";
+
+// switch(color){
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "yellow":
+//         console.log("go slow");
+//         break;
+//     case "green":
+//         console.log("go");
+//         break;
+//     default:
+//         console.log("fault");
+// }
+
+// let dayNum = 1;
+
+// switch (dayNum) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("day no present");
+// }
+
+// alert("something is wrong");
+// console.error("this is error");
+// console.warning("this is warning");
+
+// prompt("get a input");

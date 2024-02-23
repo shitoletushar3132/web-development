7d9d61dfbecc3d92ebacc95f97a713643fe643da
Tushar shitole <153802284+shitoletushar3132@users.noreply.github.com>
2024-02-23 09:26:58 +0530

    Add files via upload

---
+++ day 16/script.js
@@ -0,0 +1,68 @@
+// let msg = "   hello     ";
+// console.log(msg);
+// msg = msg.trim();
+// console.log(msg);
+
+// let name = "tusHaR";
+// name.toUpperCase();
+// name.toLowerCase();
+
+
+// let msg = "ILoveCoding";
+// msg.indexOf("Love");
+// msg.indexOf('o');
+
+
+// let msg = "           hello         ";
+// let newmsg = msg.trim().toUpperCase(); //method chaining
+
+
+// let msg = "ILoveCoding";
+// msg.slice(5);
+// msg.slice(1,4);
+// // msg.slice(-1) = msg.slice(msg.length-(index))
+
+// let msg = "ILoveCoding";
+// msg.replace("Love","Do");
+// msg.replace('o','x');
+// msg.replaceAll('o','x');
+
+
+//msg.repeat(2);
+
+
+let students = ["kunal","abi","tushar"];
+// students[1];
+// students.push("om");
+// students.unshift("chetan");
+// students.pop();
+// students.shift();
+
+// let followers = ['a','b','c','d'];
+// let blocked = [followers.shift()];
+
+// let months = ['january','july','march','august'];
+// months.indexOf('january');
+// months.includes('march');
+
+// let newMonths = ['april','september'];
+// months.concats(newMonths);
+// newmonths.reverse();
+
+
+let colors = ['red','yellow','blue','orange','pink','white'];
+// colors.slice();
+// colors.slice(2,4);
+// colors.slice(-2);
+
+
+// colors.splice(4);
+// colors.splice(0,1);
+
+//colors.sort();
+
+let months = ['january','july','march','august'];
+months.splice(0,2,"july","june"); //['july', 'june', 'march', 'august']
+
+
+let tic = [['x',null,'o'],[null,'x',null],['o',null,'x']];
